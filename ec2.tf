9d85f89eb2be7e5bee1a20bf65824f7b3cd43f95
Lion Technologies <78097736+LTech1@users.noreply.github.com>
2021-04-27 21:06:41 -0400

    fir commit

---
+++ ec2.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "server1" {
+    ami = "xxx"
+    instance_type = "t2.micro"
+    key_name  =  "key"
+    tags  = {
+        Name = "dbserver"
+        Environment = "dev"
+        Team = "Developers"
+    }
+  
+}
